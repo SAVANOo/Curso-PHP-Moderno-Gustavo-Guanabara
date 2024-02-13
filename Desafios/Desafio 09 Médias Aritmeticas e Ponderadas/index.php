d789cb6d5ab48607ccc2e1a3f341577a925a23d2
SAVANOo <savio_gato1000@hotmail.com>
2024-02-13 15:29:03 -0300

    Add: Desafio 09

---
+++ Desafios/Desafio 09 Médias Aritmeticas e Ponderadas/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Médias Aritmeticas e Ponderadas</title>
+</head>
+
+<body>
+    <?php
+    $primeiroNumero =  $_GET['primeiroNumero'] ?? 0;
+    $segundoNumero =  $_GET['segundoNumero'] ?? 0;
+    $pesoPrimeiroNumero = $_GET['pesoPrimeiroNumero'] ?? 1;
+    $pesoSegundoNumero = $_GET['pesoSegundoNumero'] ?? 1;
+
+    $mediaAritmetica = ($primeiroNumero + $segundoNumero) / 2;
+    $mediaPonderada = (($primeiroNumero * $pesoPrimeiroNumero) + ($segundoNumero * $pesoSegundoNumero)) / 2;
+
+    ?>
+
+    <section>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="numero">Primeiro Numero</label>
+            <input type="number" name="primeiroNumero" value="<?= $primeiroNumero ?>" step="any" required>
+            <label for="numero">Peso do Primeiro Numero</label>
+            <input type="number" name="pesoPrimeiroNumero" value="<?= $pesoPrimeiroNumero ?>" step="any" required>
+            <label for="numero">Segundo Numero</label>
+            <input type="number" name="segundoNumero" value="<?= $segundoNumero ?>" step="any" required>
+            <label for="numero">Peso Segundo Numero</label>
+            <input type="number" name="pesoSegundoNumero" value="<?= $pesoSegundoNumero ?>" step="any" required>
+            <input type="submit" value="Calcular">
+        </form>
+    </section>
+
+    <?php
+    if (isset($_GET['primeiroNumero']) && isset($_GET['pesoPrimeiroNumero']) && isset($_GET['segundoNumero']) && isset($_GET['pesoSegundoNumero'])) {
+        echo "<section>
+            <h1>Resultado</h1>" . "
+                <ul>
+                    <li>Media Aritmetica $mediaAritmetica</li>
+                    <li>Media Ponderada $mediaPonderada</li>
+                </ul>" . "
+            </section>";
+    };
+    ?>
+</body>
+
+</html>
---
+++ Desafios/Desafio 10 Calculando Idade/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Salário Minimo</title>
+</head>
+
+<body>
+    <?php
+    $salario =  $_GET['salario'] ?? 0;
+    $salariominimo = 1380;
+    $qtdsalarios = intdiv($salario, $salariominimo);
+    $resto = $salario % $salariominimo;
+
+    ?>
+
+    <section>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="salario">Salário</label>
+            <input type="number" name="salario" value="<?= $salario ?>" required>
+            <input type="range" name="RangeSalarial" id="IdRangeSalarial" min="0" max="100">
+        </form>
+    </section>
+    <?php
+    if (isset($_GET['salario'])) {
+        echo "<section>
+            <h1>Resultado</h1>
+            <h3>" . number_format($salario, 2, ",", ".")  . " reais equivalem a " . $qtdsalarios  . " salários minimos + " . number_format($resto, 2, ",", ".")  . " reais</h3>
+            </section>";
+    };
+    ?>
+</body>
+
+</html>
