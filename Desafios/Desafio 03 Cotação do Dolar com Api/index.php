aeb39b690e5ca6356494a3c1972b5ba2234cd630
SAVANOo <savio_gato1000@hotmail.com>
2024-02-16 16:30:30 -0300

    rm:arquivos css desnecessários

--- Desafios/Desafio 03 Cotação do Dolar com Api/index.php
+++ Desafios/Desafio 03 Cotação do Dolar com Api/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conversão de Moeda</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../style.css">
 </head>
 
 <body>
--- Desafios/Desafio 08 Raiz Quadrada e Cubica/index.php
+++ Desafios/Desafio 08 Raiz Quadrada e Cubica/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../style.css">
     <title>Calculador de Raízes Quadradas e Cubicas</title>
 </head>
 
